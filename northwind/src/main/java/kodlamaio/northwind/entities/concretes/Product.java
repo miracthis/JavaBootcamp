987087c664c75c9a7b56013ebbae42cf16b54d96
miracthis <mirac@matabt.com>
2021-05-22 23:55:31 +0300

    Day 7 / End

--- northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -3,6 +3,7 @@ package kodlamaio.northwind.entities.concretes;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -18,7 +19,7 @@ import lombok.NoArgsConstructor;
 public class Product {
 	
 	    @Id
-	    @GeneratedValue
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	    @Column(name="product_id")
 		private int id;
 	    
@@ -37,11 +38,5 @@ public class Product {
 	    @Column(name="quantity_per_unit")
 		private String quantityPerUnit;
 	    
-	   
-	
-		
-		
-		
-		
-		
+	   		
 }
